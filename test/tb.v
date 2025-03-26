ade04510b26a7017b35f7b188db86f45beaa06c9
Melika Morsali Toshmanloui <111704438+mmt60@users.noreply.github.com>
2025-03-26 16:31:06 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -16,12 +16,7 @@ module tb ();
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  reg out;
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
@@ -36,12 +31,7 @@ module tb ();
       .VGND(VGND),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
+     .uo_out (out),   // Dedicated outputs
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
